aced10e3a94fed73dfae15d4c015c8e9fffe307a
dcsturman <dcsturman@users.noreply.github.com>
2024-12-17 14:32:25 -0800

    Coverage for lib and main
    Lib is well covered.
    Main is well covered but seems there are functions due to args that aren't.
    
    Authentication is for a later date - not sure how to build tests there.

--- callisto/run_coverage.sh
+++ callisto/run_coverage.sh
@@ -11,3 +11,24 @@ cargo build # Build rust binaries.
 # ...
 
 cargo llvm-cov report
+
+cargo test
+
+# Run the server a few times to get coverage of command line args
+export RUST_LOG=info
+echo "Running server a few times to get coverage of command line args. Logging = info"
+echo "Test help"
+./target/debug/callisto --help
+echo "Test version"
+./target/debug/callisto --version
+echo "Test long flags"
+cargo run -- --port 4100  --scenario_file ./tests/test_scenario.json --design_file ./tests/test_ship_templates.json --users_file ./config/authorized_users.json --web-server http://localhost:50001 &
+echo "Test long test flag"
+cargo run -- --port 4101  --test &
+
+echo "Kill all the test servers."
+kill %1 %2
+
+cargo llvm-cov report
+cargo llvm-cov report --lcov --output-path=coverage/tests.lcov
+
