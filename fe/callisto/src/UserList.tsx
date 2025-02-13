0cc00c11e5884ad8d67a121a6b22fca29d16e37e
dcsturman <dcsturman@users.noreply.github.com>
2025-02-12 21:04:44 -0800

    Hide user list when only one users (yourself)

--- fe/callisto/src/UserList.tsx
+++ fe/callisto/src/UserList.tsx
@@ -3,14 +3,15 @@ import * as React from "react";
 export type UserList = [string];
 
 export function Users(args: {users: UserList, email: string | null}) {
+    console.log("Length of list = " + args.users.length);
     return (
-        <div className="user-list">
+        args.users.length > 1 ? <div className="user-list">
             <h4>Users</h4>
             <ul className="user-list-list">
                 {args.users.filter(user => user !== args.email).map((user) => (
                     <li key={user}>{user}</li>
                 ))}
             </ul>
-        </div>
+        </div> : <></>
     );
 };
