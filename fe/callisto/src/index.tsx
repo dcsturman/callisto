9f96c6e30a4a2efe521090b6acfb2c136868098f
dcsturman <dcsturman@users.noreply.github.com>
2025-07-18 19:55:30 -0400

    Remove Tutorial env variable (no longer needed)

--- fe/callisto/src/index.tsx
+++ fe/callisto/src/index.tsx
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom/client";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 import "./index.css";
 import { GOOGLE_OAUTH_CLIENT_ID, App } from "./App";
-import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
@@ -22,11 +21,6 @@ if (process.env.REACT_APP_CALLISTO_BACKEND) {
 }
 
 console.log("Running on " + window.location.href);
-if (process.env.REACT_APP_RUN_TUTORIAL) {
-  console.log("Tutorial is set to run.");
-} else {
-  console.log("Tutorial is not set to run.");
-}
 console.groupEnd();
 
 root.render(
@@ -36,8 +30,3 @@ root.render(
     </React.StrictMode>
   </GoogleOAuthProvider>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
